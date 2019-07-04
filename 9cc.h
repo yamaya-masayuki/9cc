e416bbc79baab2b0f5046c59d33c19808d875636
Masayuki Yamaya <yamaya.masayuki@classmethod.jp>
2019-07-04 18:59:27 +0900

    Separate sources

---
+++ 9cc.h
@@ -0,0 +1,16 @@
+typedef struct Node {
+    int ty;           // 演算子かND_NUM
+    struct Node *lhs; // 左辺
+    struct Node *rhs; // 右辺
+    int val;          // tyがND_NUMの場合のみ使う
+} Node;
+
+enum {
+    ND_NUM = 256,     // 整数のノードの型
+};
+
+extern char *user_input;
+extern void tokenize();
+extern void error_at(char *loc, char *msg);
+extern Node *expr();
+extern void gen(Node *node);
