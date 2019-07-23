a10058d18e71a4801b0baf992e14e51cff647a5e
Masayuki Yamaya <yamaya.masayuki@classmethod.jp>
2019-07-23 18:27:06 +0900

    Implement NodeKind

--- 9cc.h
+++ 9cc.h
@@ -1,13 +1,18 @@
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
 typedef struct Node {
-    int ty;           // 演算子かND_NUM
+    NodeKind kind;    // 演算子かND_NUM
     struct Node *lhs; // 左辺
     struct Node *rhs; // 右辺
-    int val;          // tyがND_NUMの場合のみ使う
+    int val;          // kindがND_NUMの場合のみ使う
 } Node;
-
-enum {
-    ND_NUM = 256,     // 整数のノードの型
-};
 
 // トークンの種類
 typedef enum {
