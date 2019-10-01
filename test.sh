136cb2fc492678e868a798c064b55db75d227293
Masayuki Yamaya <yamaya.masayuki@classmethod.jp>
2019-10-01 19:22:52 +0900

    :sparkles: while文のサポート
    ジーニアス!

--- test.sh
+++ test.sh
@@ -52,5 +52,7 @@ try 3 '{ x = 1; y = 2; return x + y; }'
 try 3 'if (1 == 1) { return 3; } else { return 6; }'
 try 6 'if (0 == 1) { return 3; } else { return 6; }'
 try 2 'if (0 == 1) { return 3; } else { a = 1; b = 0; return (a + b) * 2; }'
+try 0 'i = 0; while (0) i = 1; return i;'
+try 5 'i = 0; while (i < 5) i = i + 1; return i;'
 
 echo OK
