ada8543d071af598f7acb0dc35fe62b2b827c4f1
Masayuki Yamaya <yamaya.masayuki@classmethod.jp>
2019-10-28 16:59:00 +0900

    add function test

--- test.sh
+++ test.sh
@@ -17,8 +17,6 @@ try() {
   fi
 }
 
-try 0 'foo();'
-exit 0
 try 0 '0;'
 try 42 '42;'
 try 21 '5+20-4;'
@@ -58,5 +56,6 @@ try 0 'i = 0; while (0) i = 1; return i;'
 try 5 'i = 0; while (i < 5) i = i + 1; return i;'
 try 8 'x = 2; for (i = 0; i < 2; i = i + 1) x = x * 2; x;'
 try 10 'x = 0; for (i = 0; i < 10; i = i + 1) { x = x + 1; } x;'
+try 0 'foo();'
 
 echo OK
